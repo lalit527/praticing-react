06185e7cbc57191a4e445b89371e14af338c78e9
lalit527 <lalit.slg007@gmail.com>
2018-05-15 21:21:56 +0530

    Adding custom modules

--- third2/src/App.js
+++ third2/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,18 +43,6 @@ class App extends Component {
 
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
     let persons = null;
     if ( this.state.showPersons ) {
       persons = (
@@ -72,26 +59,20 @@ class App extends Component {
       );
     }
 
-    style.backgroundColor = 'red';
-    style[':hover'] = {
-      backgroundColor: 'salmon',
-      color: 'black'
-    }
-
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join( ' ' )}>This is really working!</p>
+        <p className={assignedClasses.join( ' ' )}>This is really working!</p>
         <button 
-          style={style}
+          className={classes.Red}
           onClick={this.togglePersonHandler}>
           Toggle Persons
         </button>
