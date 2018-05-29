c3f58b550327f896cb0f3d6cf46f7618c27f46cd
lalit527 <lalit.slg007@gmail.com>
2018-05-29 11:46:49 +0530

    Completed Routing part2

--- routing-02/src/containers/Blog/NewPost/NewPost.js
+++ routing-02/src/containers/Blog/NewPost/NewPost.js
@@ -7,7 +7,11 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Lalit'
+    }
+
+    componentDidMount() {
+        console.log(this.props);
     }
 
     postDataHandler = () => {
@@ -32,8 +36,8 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Jon">Jon</option>
+                    <option value="Doe">Doe</option>
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>
