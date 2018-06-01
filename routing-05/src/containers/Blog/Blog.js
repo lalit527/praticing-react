c366a17e52451e05992208bdd4ebc2ff3cb1609f
lalit527 <lalit.slg007@gmail.com>
2018-06-01 22:56:13 +0530

    Completed till routing

--- routing-05/src/containers/Blog/Blog.js
+++ routing-05/src/containers/Blog/Blog.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({auth: true});
+        }, 10000)
+    }
+   
+
     render () {
         return (
             <div className="Blog">
@@ -30,7 +45,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/new-post" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
