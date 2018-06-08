4abfc00d58a2bc9d063c317637fb0db8a2b657aa
lalit527 <lalit.slg007@gmail.com>
2018-06-08 12:05:06 +0530

    Http part 2

--- http-burger-01/src/containers/BurgerBuilder/BurgerBuilder.js
+++ http-burger-01/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import WithErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -139,4 +140,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default WithErrorHandler( BurgerBuilder, axios );
