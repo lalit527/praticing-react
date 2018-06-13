dc44c08a47eaf08fb1f157030cfa58a8e0359983
lalit527 <lalit.slg007@gmail.com>
2018-06-13 15:35:45 +0530

    Added form handling part1

---
+++ form-burger-01/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter >
+        <div>
+          <Layout>
+            <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/orders" component={Orders} />
+              <Route path="/" exact component={BurgerBuilder} />
+            </Switch>          
+          </Layout>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
